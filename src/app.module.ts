0a6b73d43156ab83e9feb9e70c9057a6c9b24b6b
Julio Hernandez <ju010970@gmail.com>
2023-07-15 14:44:18 -0600

    Primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
+import * as process from "process";
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASS,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ProductModule,
+  ],
+})
+export class AppModule {}
